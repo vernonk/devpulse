e7bd18f5b19ac55aee6500e4aca41fc89ce28486
Vernon Kesner <vernon.kesner@bottomline.com>
2024-05-16 17:19:25 -0400

    fix: Move Jira auth headers to constant

--- app/utils/jira.server.ts
+++ app/utils/jira.server.ts
@@ -1,10 +1,12 @@
 import { json } from "@remix-run/node";
 
+const COMMON_HEADERS = {
+  Authorization: `Bearer ${process.env.JIRA_API_TOKEN}`,
+};
+
 export const getMyPermissions = async () => {
   const response = await fetch(`${process.env.BASE_JIRA_API_URL}/mypermissions`, {
-    headers: {
-      Authorization: `Bearer ${process.env.JIRA_API_TOKEN}`,
-    },
+    headers: COMMON_HEADERS,
   });
   return json(await response.json());
 };
