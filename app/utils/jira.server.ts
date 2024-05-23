8c0c584f23406c1ad8565479a588326f7920cbde
Vernon Kesner <vernon.kesner@bottomline.com>
2024-05-23 15:57:42 -0400

    feat: Add server call to get user tickets for duration

--- app/utils/jira.server.ts
+++ app/utils/jira.server.ts
@@ -1,4 +1,5 @@
-import { json } from '@remix-run/node';
+// TODO: Will be removed for db values, temporary for testing APIs
+import devpulseConfig from '../../devpulse.config';
 
 const COMMON_HEADERS = {
   Authorization: `Bearer ${process.env.JIRA_API_TOKEN}`,
@@ -8,7 +9,19 @@ const get = async (url: string) => {
   const response = await fetch(url, {
     headers: COMMON_HEADERS,
   });
-  return json(await response.json());
+  return await response.json();
+}
+
+const post = async (url: string, body: any) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      ...COMMON_HEADERS,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  });
+  return await response.json();
 }
 
 export const getMyPermissions = async () => {
@@ -19,4 +32,36 @@ export const getUserData = async (query: string) => {
   const endpoint = `${process.env.BASE_JIRA_API_URL}/user/picker`;
   const search = `?query=${query}&maxResults=10&showAvatar=true`;
   return await get(`${endpoint}${search}`);
+}
+
+const defaultUserTicketsForDurationQuery = {
+  assignee: '',
+  duration: '-30days',
+};
+
+export const getUserTicketsForDuration = async (
+  options = defaultUserTicketsForDurationQuery
+) => {
+  const endpoint = `${process.env.BASE_JIRA_API_URL}/search`;
+  // TODO: Smarten up jql when moving these to db driven values
+  const {
+    JIRA: {
+      TICKETS_FOR_DURATION: {
+        IGNORED_ISSUE_TYPES,
+        MAX_RESULTS,
+        ORDER_BY,
+      },
+    },
+  } = devpulseConfig;
+  const jql = `assignee was ${options.assignee} AND updated >= ${options.duration} AND issuetype not in (${IGNORED_ISSUE_TYPES.join(', ')}) ORDER BY ${ORDER_BY.join(', ')}`;
+  return await post(`${endpoint}`, {
+    jql,
+    startAt: 0,
+    maxResults: MAX_RESULTS,
+    fields: [
+      'summary',
+      'status',
+      'assignee',
+    ],
+  });
 }
