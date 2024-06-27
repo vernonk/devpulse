a5b8465ba9fe684040a2f590df75df78e1b03576
Vernon Kesner <vernon.kesner@bottomline.com>
2024-06-27 15:05:31 -0400

    Move all the base styles up to the root

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import type { LinksFunction, MetaFunction } from '@remix-run/node';
 import {
   Links,
   Meta,
@@ -6,6 +7,25 @@ import {
   ScrollRestoration,
 } from '@remix-run/react';
 import { ColorSchemeScript, MantineProvider } from '@mantine/core';
+
+import coreStylesHref from '@mantine/core/styles.css?url';
+import chartsStylesHref from '@mantine/charts/styles.css?url';
+import notificationsStylesHref from '@mantine/notifications/styles.css?url';
+import datesStylesHref from '@mantine/dates/styles.css?url';
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: 'DevPulse' },
+    { name: 'description', content: 'Stay on the pulse of your team' },
+  ];
+};
+
+export const links: LinksFunction = () => [
+  { rel: 'stylesheet', href: coreStylesHref },
+  { rel: 'stylesheet', href: chartsStylesHref },
+  { rel: 'stylesheet', href: datesStylesHref },
+  { rel: 'stylesheet', href: notificationsStylesHref },
+];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,24 +1,3 @@
-import type { LinksFunction, MetaFunction } from '@remix-run/node';
-
-import coreStylesHref from '@mantine/core/styles.css?url'; 
-import chartsStylesHref from '@mantine/charts/styles.css?url';
-import notificationsStylesHref from '@mantine/notifications/styles.css?url';
-import datesStylesHref from '@mantine/dates/styles.css?url';
-
-export const meta: MetaFunction = () => {
-  return [
-    { title: 'DevPulse' },
-    { name: 'description', content: 'Stay on the pulse of your team' },
-  ];
-};
-
-export const links: LinksFunction = () => [
-  { rel: 'stylesheet', href: coreStylesHref },
-  { rel: 'stylesheet', href: chartsStylesHref },
-  { rel: 'stylesheet', href: datesStylesHref },
-  { rel: 'stylesheet', href: notificationsStylesHref },
-];
-
 export default function Index() {
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
