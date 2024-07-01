05ac72be897562c0a05b85cd74844e64ad5f1be8
Vernon Kesner <vernon.kesner@bottomline.com>
2024-07-01 18:28:14 -0400

    Add Time In Status Columns to metrics table

--- app/routes/insights/MetricsTable.jsx
+++ app/routes/insights/MetricsTable.jsx
@@ -1,16 +1,24 @@
 import PropTypes from 'prop-types';
-
 import { Table } from '@mantine/core';
 
 import StatusBadge from './StatusBadge';
 
-export default function MetricsTable({ data }) {
+// TODO... update query to expand on changelog in initial query may be able to get all that data in onego
+// IT WORKS :) Update logic to remove details button, add data in initial data set
+
+export default function MetricsTable({
+  data,
+}) {
+  console.log('data', data);
   const rows = data.issues.map((issue) => (
     <Table.Tr key={issue.key}>
-      <Table.Td>
-        <span style={{ textWrap: 'nowrap' }}>{issue.key}</span>
+      <Table.Td className="nowrap">
+        <a href={issue.link} target="_blank" rel="noreferrer">{issue.key}</a>
       </Table.Td>
       <Table.Td><StatusBadge status={issue.fields.status.name} /></Table.Td>
+      <Table.Td className="nowrap">{issue.timeInProgress}</Table.Td>
+      <Table.Td className="nowrap">{issue.timeInValidation}</Table.Td>
+      <Table.Td className="nowrap">{issue.timeInFixIssues}</Table.Td>
       <Table.Td>{issue.fields.summary}</Table.Td>
     </Table.Tr>
   ));
@@ -19,8 +27,11 @@ export default function MetricsTable({ data }) {
     <Table highlightOnHover striped>
       <Table.Thead>
         <Table.Tr>
-          <Table.Th>Issue Key</Table.Th>
+          <Table.Th>Issue</Table.Th>
           <Table.Th>Status</Table.Th>
+          <Table.Th>Time In Progress</Table.Th>
+          <Table.Th>Time In Validation</Table.Th>
+          <Table.Th>Time In Fix Issues</Table.Th>
           <Table.Th>Summary</Table.Th>
         </Table.Tr>
       </Table.Thead>
@@ -30,5 +41,15 @@ export default function MetricsTable({ data }) {
 }
 
 MetricsTable.propTypes = {
-  data: PropTypes.array.isRequired,
+  data: PropTypes.shape({
+    issues: PropTypes.arrayOf(PropTypes.shape({
+      key: PropTypes.string.isRequired,
+      fields: PropTypes.shape({
+        status: PropTypes.shape({
+          name: PropTypes.string.isRequired,
+        }).isRequired,
+        summary: PropTypes.string.isRequired,
+      }).isRequired,
+    }).isRequired).isRequired,
+  }).isRequired,
 };
